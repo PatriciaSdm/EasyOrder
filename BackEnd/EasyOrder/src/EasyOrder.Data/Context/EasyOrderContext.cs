637b03d480911d97f84c4169546643cd5c4d2516
Patricia <Patricia@LABSIT-PATRICIA>
2021-09-08 22:57:58 -0300

    Adicionado Contexto Instalado pacote Microsoft.EntityFrameworkCore

---
+++ BackEnd/EasyOrder/src/EasyOrder.Data/Context/EasyOrderContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Data.Context
+{
+    public class EasyOrderContext : DbContext
+    {
+        public EasyOrderContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
