f5f4bb9927452cf9392b5567443b2604bccda362
Patricia <Patricia@LABSIT-PATRICIA>
2021-09-09 14:32:56 -0300

    Adicionado serviço e interfaces de serviço

---
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Services/CategoryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Business.Services
+{
+    public class CategoryService : Service
+    {
+       
+    }
+}
---
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Services/Service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Business.Services
+{
+    public abstract class Service
+    {
+       
+    }
+}
