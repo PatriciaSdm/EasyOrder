d0b2fe989cb79f2bcf10c413a4f91795fc1c0365
Patricia <Patricia@LABSIT-PATRICIA>
2021-09-08 23:28:44 -0300

    Install-package Microsoft.EntityFrameworkCore.Relational Iniciando configurações para o mapeamento das entidades

---
+++ BackEnd/EasyOrder/src/EasyOrder.Data/Mappings/CategoryMapping.cs
@@ -0,0 +1,22 @@
+﻿using EasyOrder.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Data.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        //Install-package Microsoft.EntityFrameworkCore.Relational
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+        }
+    }
+}
---
+++ BackEnd/EasyOrder/src/EasyOrder.Data/Mappings/ExtraMapping.cs
@@ -0,0 +1,21 @@
+﻿using EasyOrder.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Data.Mappings
+{
+    public class ExtraMapping : IEntityTypeConfiguration<Extra>
+    {
+        public void Configure(EntityTypeBuilder<Extra> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+        }
+    }
+}
