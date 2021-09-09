7fc50a4fda05bcf790b6e2e1194f5a8fdb89de96
Patricia <Patricia@LABSIT-PATRICIA>
2021-09-09 14:38:22 -0300

    Instalado Fluent Validation e criado classes de validação

---
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Models/Validations/CategoryValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Business.Models.Validations
+{
+    public class CategoryValidation : AbstractValidator<Category>
+    {
+        public CategoryValidation()
+        {
+            
+        }
+    }
+}
---
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Models/Validations/ExtraValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOrder.Business.Models.Validations
+{
+    public class ExtraValidation : AbstractValidator<Extra>
+    {
+        public ExtraValidation()
+        {
+            
+        }
+    }
+}
