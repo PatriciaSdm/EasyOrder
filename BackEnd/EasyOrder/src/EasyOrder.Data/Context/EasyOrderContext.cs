faf7e5c997ee871d276de8f02a2bbbb55b3cd782
Patricia <Patricia@LABSIT-PATRICIA>
2021-09-09 14:06:54 -0300

    Adicionado configurações do OnModelCreating

--- BackEnd/EasyOrder/src/EasyOrder.Data/Context/EasyOrderContext.cs
+++ BackEnd/EasyOrder/src/EasyOrder.Data/Context/EasyOrderContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EasyOrder.Data.Context
@@ -18,5 +19,20 @@ namespace EasyOrder.Data.Context
         public DbSet<Item> Items { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EasyOrderContext).Assembly);
+
+            //Desabilita delete cascade
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
