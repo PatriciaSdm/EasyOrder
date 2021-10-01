8e94bc60882f738e7d4c0218a2b8b105f4f9e562
Felipe Guizzo <felipe.guizzo@labsit.io>
2021-10-01 19:28:06 -0300

    Definindo métodos do serviço da comanda

--- BackEnd/EasyOrder/src/EasyOrder.Business/Interfaces/Services/IOrderService.cs
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Interfaces/Services/IOrderService.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using EasyOrder.Business.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EasyOrder.Business.Interfaces.Services
 {
     public interface IOrderService : IDisposable
     {
-
+        Task<bool> Include(Order order);
+        Task<bool> Update(Order order);
+        Task<bool> Close(Order order);
+        Task<Order> GetById(Guid id);
+        Task<Order> GetByIdWithItens(Guid id);
+        Task<List<Order>> GetAll();
     }
 }
--- BackEnd/EasyOrder/src/EasyOrder.Business/Services/OrderService.cs
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Services/OrderService.cs
@@ -1,8 +1,10 @@
 ﻿using EasyOrder.Business.Interfaces.INotifications;
 using EasyOrder.Business.Interfaces.Services;
+using EasyOrder.Business.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EasyOrder.Business.Services
 {
@@ -12,8 +14,38 @@ namespace EasyOrder.Business.Services
         {
 
         }
-    
+
+        public async Task<bool> Close(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Order>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Order> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Order> GetByIdWithItens(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Include(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(Order order)
         {
             throw new NotImplementedException();
         }
