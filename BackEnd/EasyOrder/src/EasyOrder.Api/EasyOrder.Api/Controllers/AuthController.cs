ec357e478559a5bb4df80d5caeffbc0b3d58f240
Felipe Guizzo <felipe.guizzo@labsit.io>
2021-10-05 19:52:59 -0300

    Implementando ASP.NET Identity

---
+++ BackEnd/EasyOrder/src/EasyOrder.Api/EasyOrder.Api/Controllers/AuthController.cs
@@ -0,0 +1,124 @@
+﻿using EasyOrder.Api.ViewModels;
+using EasyOrder.Business.Interfaces.INotifications;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyOrder.Api.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotifier notifier,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notifier)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("new-account")]
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(/*await GerarJwt(user.Email)*/);
+            }
+            foreach (var error in result.Errors)
+            {
+                NotifyError(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true); //5 vezes
+
+            if (result.Succeeded)
+            {
+                //_logger.LogInformation("Usuario " + loginUser.Email + " logado com sucesso");
+                return CustomResponse(/*await GerarJwt(loginUser.Email)*/);
+            }
+            if (result.IsLockedOut)
+            {
+                NotifyError("Usuário temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse(loginUser);
+            }
+
+            NotifyError("Usuário ou Senha incorretos");
+            return CustomResponse(loginUser);
+        }
+
+        //private async Task<LoginResponseViewModel> GerarJwt(string email)
+        //{
+        //    var user = await _userManager.FindByEmailAsync(email);
+        //    var claims = await _userManager.GetClaimsAsync(user);
+        //    var userRoles = await _userManager.GetRolesAsync(user);
+
+        //    claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+        //    claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        //    claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        //    claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+        //    claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+        //    foreach (var userRole in userRoles)
+        //    {
+        //        claims.Add(new Claim("role", userRole));
+        //    }
+
+        //    var identityClaims = new ClaimsIdentity();
+        //    identityClaims.AddClaims(claims);
+
+        //    var tokenHandler = new JwtSecurityTokenHandler();
+        //    var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+        //    var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        //    {
+        //        Issuer = _appSettings.Emissor,
+        //        Audience = _appSettings.ValidoEm,
+        //        Subject = identityClaims,
+        //        Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+        //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        //    });
+
+        //    var encodedToken = tokenHandler.WriteToken(token);
+
+        //    var response = new LoginResponseViewModel
+        //    {
+        //        AccessToken = encodedToken,
+        //        ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+        //        UserToken = new UserTokenViewModel
+        //        {
+        //            Id = user.Id,
+        //            Email = user.Email,
+        //            Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+        //        }
+        //    };
+
+        //    return response;
+        //}
+
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+    }
+}
