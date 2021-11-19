6aa8edd10c353219781d395f794f45da80349c5a
patriciaSdm <patricia_dellamatta@hotmail.com>
2021-11-19 10:36:05 -0300

    Corrigido Updateb de adicionais

--- BackEnd/EasyOrder/src/EasyOrder.Business/Models/Extra.cs
+++ BackEnd/EasyOrder/src/EasyOrder.Business/Models/Extra.cs
@@ -11,7 +11,7 @@ namespace EasyOrder.Business.Models
 
 
         /* EF Relations */
-        public ICollection<CategoryExtra> CategoryExtras { get; set; }
+        public List<CategoryExtra> CategoryExtras { get; set; }
         //public virtual ICollection<Item> Items { get; set; }
     }
 }
